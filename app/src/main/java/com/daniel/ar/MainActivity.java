6c12f7603acc24fd217d3eae01f467cb7b8d3b99
Danielcorzo1996 <corzodaniel492@gmail.com>
2024-03-30 17:17:52 -0500

    Crud ApiRest

---
+++ app/src/main/java/com/daniel/ar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.daniel.ar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonIrASegundoLayout = findViewById(R.id.btnAgregar);
+
+        botonIrASegundoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
